20251b57f5f6543a5141ce9f150c8cdcd7407c4a
Yazeed Almuqwishi <yazeed.almuqwishi@gmail.com>
2022-06-14 21:23:59 +0300

    Added deployment

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
-// migrating the appropriate contracts
-var SquareVerifier = artifacts.require("./SquareVerifier.sol");
-var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
+const Verifier = artifacts.require("Verifier");
+const SolnSquareVerifier = artifacts.require("SolnSquareVerifier");
 
-module.exports = function(deployer) {
-  deployer.deploy(SquareVerifier);
-  deployer.deploy(SolnSquareVerifier);
+module.exports = async function (deployer) {
+    // deployer.deploy(SquareVerifier);
+    // deployer.deploy(SolnSquareVerifier);
+    // deployer.deploy(PropertyERC721Token, "Property NFT", "PRP");
+    await deployer.deploy(Verifier);
+    await deployer.deploy(SolnSquareVerifier, "Property", "PRP", Verifier.address);
 };
